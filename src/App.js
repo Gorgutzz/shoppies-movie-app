1df9259b814e0326db6f9ba19dde7da1bca66f56
“Gorgutzz” <leo.kalshteyn@gmail.com>
2021-05-08 14:55:33 -0700

    add call function to API in app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
 
 const App = () => {
-	const [movies, setMovies] = useState([
+	const [movies, setMovies] = useState([]);
 
-]);
+	const getMovieRequest = async () => {
+		const url = `https://www.omdbapi.com/?s=man&apikey=4a3b711b`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest();
+	}, []);
 
 	return (
 		<div className='container-fluid movie-app'>
 			<div className='row'>
-
+				<MovieList movies={movies} />
 			</div>
 		</div>
 	);
